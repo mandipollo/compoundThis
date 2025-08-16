cbd3cc3349433d9f99bc39193dbfe07e09f4c4b6
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2025-08-16 16:48:00 +0100

    test(resendVerificationCode)- added test for resendVerificationCode aws wrapper

---
+++ src/libs/cognito/newUser/resenVerificationCode.test.ts
@@ -0,0 +1,67 @@
+// mock
+
+vi.mock("aws-amplify/auth", () => ({
+	resendSignUpCode: vi.fn(),
+}));
+
+const mockedResendSignUpCode = vi.mocked(resendSignUpCode);
+// imports
+
+import { resendVerificationCode } from "./resendVerificationCode";
+import { resendSignUpCode } from "aws-amplify/auth";
+import { vi, expect, describe, afterEach, it } from "vitest";
+
+// clean ups
+afterEach(() => {
+	vi.clearAllMocks();
+});
+
+//
+
+describe("aws resendSignUpCode wrapper function ", () => {
+	it("resendSignUpCode correctly resolves", async () => {
+		mockedResendSignUpCode.mockResolvedValue({
+			deliveryMedium: "EMAIL",
+			destination: "test@example.com",
+		});
+
+		const { error, success, message, result } =
+			await resendVerificationCode("test@example.com");
+
+		//
+
+		expect(error).toEqual("");
+		expect(success).toEqual(true);
+		expect(message).toEqual("Verification code emailed successfully");
+	});
+
+	// error scenarios
+
+	it("should return User does not exist on UserNotFoundException", async () => {
+		mockedResendSignUpCode.mockRejectedValue({ name: "UserNotFoundException" });
+
+		const { error, success } = await resendVerificationCode("test@example.com");
+
+		expect(error).toEqual("User does not exist");
+		expect(success).toEqual(false);
+	});
+	it("should return Failed to deliver the code on CodeDeliveryFailureException", async () => {
+		mockedResendSignUpCode.mockRejectedValue({
+			name: "CodeDeliveryFailureException",
+		});
+
+		const { error, success } = await resendVerificationCode("test@example.com");
+
+		expect(error).toEqual("Failed to deliver the code");
+		expect(success).toEqual(false);
+	});
+
+	it("should return Something went wrong, Please try again on Undefined error", async () => {
+		mockedResendSignUpCode.mockRejectedValue({});
+
+		const { error, success } = await resendVerificationCode("test@example.com");
+
+		expect(error).toEqual("Something went wrong. Please try again");
+		expect(success).toEqual(false);
+	});
+});
--- src/libs/cognito/newUser/resendVerificationCode.ts
+++ src/libs/cognito/newUser/resendVerificationCode.ts
@@ -13,7 +13,7 @@ export async function resendVerificationCode(email: string) {
 			message: "Verification code emailed successfully",
 		};
 	} catch (error: any) {
-		let errorMessage = "Something went wrong";
+		let errorMessage = "Something went wrong. Please try again";
 
 		switch (error.name) {
 			case "UserNotFoundException":
@@ -23,7 +23,7 @@ export async function resendVerificationCode(email: string) {
 				errorMessage = "Failed to deliver the code";
 				break;
 			default:
-				errorMessage = "Something went wrong";
+				errorMessage = "Something went wrong. Please try again";
 		}
 
 		return {
